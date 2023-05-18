c71311d659872654abe499e41563926cfcc3fc42
hector994 <50502676+hector994@users.noreply.github.com>
2023-05-18 12:39:40 -0600

    la base de datos
    solo ejecuta

---
+++ base de datos estudiantes.sql
@@ -0,0 +1,14 @@
+go
+create database dbServicioEstudiantil;
+go
+
+use dbServicioEstudiantil
+go
+create table estudiantes(
+id int, 
+nombre varchar(50),
+apellido varchar(50),
+telefono varchar(50)
+)
+go
+
